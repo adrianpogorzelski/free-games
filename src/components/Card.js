f350817eee229ac073c0665a2dc0cee204ce7bd3
a.pogorzelski <pogorzelski.adrian@gmail.com>
2023-10-23 11:59:18 +0200

    Link fixes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {useDispatch} from "react-redux";
-import {setCurrentGameId} from "../store/gameDetailsSlice";
+import { setCurrentGameId} from "../store/gameDetailsSlice";
 
 const Card = ({id, thumbnail, title, genre, platform, short_description}) => {
     const dispatch = useDispatch();
